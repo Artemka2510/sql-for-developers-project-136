3de437690e41122f930f59933ded654062aebb5f
Artemka2510 <119489875+Artemka2510@users.noreply.github.com>
2026-04-16 16:34:58 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,37 @@
 create table Lessons (
-  id
-  name
-  lesson_content
-  link_on_video
-  position
-  create_data
-  update_data
-  link_on_course not null
+  id  PRIMARY KEY,
+  lesson_name varchar(50),
+  lesson_content varchar(50),
+  link_on_video varchar(50),
+  position int not null,
+  create_data date,
+  update_data date,
+  link_on_course not null,
   deleted boolean
+)
+
+create table Courses (
+  id  PRIMARY KEY,
+  course_name varchar(50),
+  course_description varchar(50),
+  create_date date,
+  update_date date
+)
+
+create table Modules (
+  id  PRIMARY KEY,
+  modul_name varchar(50),
+  modul_description varchar(50),
+  create_date date,
+  update_date date
+)
+
+create table Programs (
+  id  PRIMARY KEY,
+  program_name varchar(50),
+  program_cost int,
+  program_type varchar(50),
+  create_date date,
+  update_date date
+  
 )
